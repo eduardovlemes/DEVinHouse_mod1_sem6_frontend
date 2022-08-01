1cbd6d36a8760e772f66fe96e1deabb0c5ac7e99
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-08-01 19:56:26 -0300

    feat: inserindo documento html

---
+++ Aulas/01_08_22/index.js
@@ -0,0 +1,13 @@
+///// Conteúdo Aula
+/* let numero = 10
+
+let texto = `O número digitado foi ${numero}`
+
+let nomeCompleto = `Cia latina de medicamentos` */
+
+
+//// Exercício em aula
+let nome = window.prompt("Digite um nome: ")
+let sobrenome = window.prompt("Digite um sobrenome: ")
+let idade = window.prompt("Digite uma idade: ")
+alert (`Eu, ${nome} ${sobrenome}, possuo ${idade} anos e nasci em ${2022 - idade}.`)
