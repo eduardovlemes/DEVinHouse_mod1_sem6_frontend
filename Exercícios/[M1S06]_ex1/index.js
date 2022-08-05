9c6d8cf13fd906f1a20bfcf81a84f7bd9df9f28e
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-08-05 13:52:50 -0300

    chore: exercício 1

---
+++ Exercícios/[M1S06]_ex1/index.js
@@ -0,0 +1,4 @@
+let nome = window.prompt("Digite um nome: ")
+let sobrenome = window.prompt("Digite um sobrenome: ")
+let idade = window.prompt("Digite uma idade: ")
+alert (`Eu, ${nome} ${sobrenome}, possuo ${idade} anos e nasci em ${2022 - idade}.`)
